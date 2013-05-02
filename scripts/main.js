8a563d2491ca2b6908ea4ae47341b0871a043d5b
Richard Bamford <rbamfordz@gmail.com>
2013-05-02 21:27:03 +0100

    fixed headcount, added queue system

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@ currentSongUri = "";   // used to make sure we don't play the same song twice.
 messageCount = 0;
 headCount = 0;
 
+var ytQueue = new Array();
+
 var wsImpl = window.WebSocket || window.MozWebSocket;
 
 // get the roomID we want to join/create
@@ -19,21 +21,34 @@ dataRef = new Firebase(firebaseRef + "/rooms/" + roomIDs);
 // get the current name of the user //
 dataRef.push({name: currentName, uri: "#J#"});
 
+
+
+//----------------- Headcounting code ----------------//
+// reference to headcount, we have to use a ifference reference or else our child_added callback breaks...
+headcountRef = new Firebase("https://spotifycampfire.firebaseio.com/roomheadcounts/"+roomIDs+"/headcount");
+headcountRef.transaction(function(current_value) {
+    headCount = headCount + 1;
+    return current_value + 1;
+});
+// this is to update our clients headcount
+headcountRef.on('value', function(dataSnapshot) {
+    $("#headcount").text(dataSnapshot.val());
+});
+//-----------------------------------------------------//
+
+
+
 // make sure we add a message to our spotify whenever we recieve one on firebase
 dataRef.limit(1).on('child_added', function(snapshot)
 {
     var message = snapshot.val();
     if(message.uri == "#J#")
     {
-        headCount = headCount + 1;
         displayChatMessage(message.name, "Has joined the room!");
-        $("#headcount").text(headCount);
     }
     else if(message.uri == "#E#")
     {
-        headCount = headCount - 1;
         displayChatMessage(message.name, "Has left the room...");
-        $("#headcount").text(headCount);
     }
     else
     {
@@ -41,23 +56,31 @@ dataRef.limit(1).on('child_added', function(snapshot)
     }
 });
 
-window.onbeforeunload = confirmExit;
 
+window.onbeforeunload = confirmExit;
 function confirmExit()
 {
-    // tell firebase we left
     dataRef.push({name: currentName, uri: "#E#"});
+    // reference to headcount
+    headcountRef.transaction(function(current_value) {
+        headCount = headCount - 1;
+        return current_value - 1;
+    });
 
-    if (headCount == 1) // last person out is a rotten egg
+    if (headCount == 0)     // last person out is a rotten egg
     {
         dataRef.remove();
+        headcountRef.remove();
     }
-
-    return "You have attempted to leave this page.  Please remember the room name if you wish to re-join.  Are you sure you want to exit this page?";
 }
+
+
 
 // update our info
 $("#tag").text(roomIDs);
+
+
+
 
 // this is where we handle all our requests
 function displayChatMessage(name, text)
@@ -74,9 +97,18 @@ function displayChatMessage(name, text)
     // Flag handling
     if (flag == "t")
     {
-        playSong(text);
-        currentSongUri = text;
-        $('#messagesDiv').append("<div><b>" + name + "</b> played a video<br /></div>");
+        if(ytQueue[0] == null)    //check if Q is empty
+        {
+            ytQueue[0] = text;
+            playSong(text);
+            currentSongUri = text;
+            $('#messagesDiv').append("<div><b>" + name + "</b> played a video<br /></div>");
+        }else       //if Q is not empty add to end of array
+        {
+            $('#messagesDiv').append("<div><b>" + name + "</b> added a video to the queue<br /></div>");
+            ytQueue.push(text);
+        }
+
     }
     else if (flag == "m")
     {
@@ -95,6 +127,7 @@ function displayChatMessage(name, text)
     }
 }
 
+
 function runClick()
 {
     var text = $('#messageInput').val();
@@ -105,7 +138,7 @@ function runClick()
         {
             if(currentSongUri != text)  // make sure it's not the same song
             {
-                dataRef.push({name: currentName, uri: strip(text)});
+                dataRef.push({name: currentName, uri: strip(text)});               
             }
             else
             {
@@ -125,6 +158,7 @@ function runClick()
     $('#messageInput').val('');           
 }
 
+
 // Parsing messages sent.
 $("#messageInput").keypress(function(e)
 {
@@ -133,3 +167,18 @@ $("#messageInput").keypress(function(e)
         runClick();
     }
 });
+
+
+
+function onytplayerStateChange(newState) {
+    // Video has ended - play next video in queue
+    if(newState == 0){
+        //check if queue is empty
+        if(ytQueue[0] != ""){
+            //.shift dat array
+            ytQueue.shift();
+            //play video at [0]
+            playSong(ytQueue[0]);
+        }
+    }
+}
